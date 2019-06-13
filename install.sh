312a30352e3a0e6c9a89e356b2f344738877f613
orion46 <earth.nobu.light@gmail.com>
2019-06-14 07:23:48 +0900

    Enable to create recursive directory

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ ! -d ~/.config/nvim ]; then
-    mkdir ~/.config/nvim
+    mkdir -p ~/.config/nvim
 fi
 cp init.vim ~/.config/nvim/init.vim
 
 if [ ! -d ~/.vim/rc ]; then
-    mkdir ~/.vim/rc
+    mkdir -p ~/.vim/rc
 fi
 cp dein.toml ~/.vim/rc/dein.toml
 cp dein_lazy.toml ~/.vim/rc/dein_lazy.toml
