cd92399c00555118a13d073e72bbd4474c68031f
orion0616 <earth.nobu.light@gmail.com>
2024-09-05 20:16:07 +0900

    feat: base.lua 完成

--- nvim/lua/config/base.lua
+++ nvim/lua/config/base.lua
@@ -14,19 +14,7 @@ vim.opt.whichwrap = 'b,s,h,l,<,>,[,]' -- カーソルを行頭行末で止まら
 vim.opt.ignorecase = true -- 大文字小文字の区別なく検索する
 vim.opt.smartcase = true -- 検索文字列に大文字が含まれている場合は区別して検索
 vim.opt.wrapscan = true -- 検索時に最後までいったら最初に戻る
+
 vim.opt.wrap = true -- ウィンドウの幅より長い行は折り返して、次の行で表示
 vim.opt.fileencoding = 'utf-8' -- 保存時の文字コード
 vim.opt.fileencodings = {'utf-8', 'euc-jp', 'iso-2022-jp', 'cp932'}
-vim.opt.hidden = true -- 保存されていないファイルがあるときも別のファイルを開くことが出来る
-vim.opt.pastetoggle = '<f5>'
-
--- autocmd
-vim.cmd([[
-  augroup MyAutoCmd
-    autocmd!
-    autocmd FileType javascript setlocal sw=2 sts=2 ts=2
-    autocmd FileType vim        setlocal sw=2 sts=2 ts=2
-    autocmd FileType yaml       setlocal sw=2 sts=2 ts=2
-    autocmd BufWritePre * :%s/\s\+$//ge
-  augroup END
-]])
